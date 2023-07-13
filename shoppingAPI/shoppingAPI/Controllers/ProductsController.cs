1bddc5707287fc313cdc82da6bf47561c6760936
vairam90 <87597007+vairam90@users.noreply.github.com>
2023-07-13 15:24:03 +0530

    Add files via upload

---
+++ shoppingAPI/shoppingAPI/Controllers/ProductsController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using shoppingAPI.Models;
+
+namespace shoppingAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductsController : ControllerBase
+	{
+		Products pObj = new Products(); //this is a bad code, we should use DI instead
+		[HttpGet]
+		[Route("/plist")]
+		public IActionResult GetAllProducts()
+		{
+			return Ok(pObj.GetAllProducts());
+		}		
+		[HttpGet]
+		[Route("/plist/name/{name}")]
+		public IActionResult GetProductsByName(string name)
+		{
+			try
+			{
+				var result = pObj.GetProductsByName(name);
+				return Ok(result);
+			}
+			catch (Exception es)
+			{
+				return NotFound(es.Message);
+			}
+		}
+		[HttpGet]
+		[Route("/plist/category/{category}")]
+		public IActionResult GetProductsByCategory(string category)
+		{
+			try
+			{
+				var result = pObj.GetProductsByCategory(category);
+				return Ok(result);
+			}
+			catch (Exception es)
+			{
+				return NotFound(es.Message);
+			}
+		}
+		[HttpGet]
+		[Route("/plist/inStock/{state}")]
+		public IActionResult GetinStockProducts(bool state)
+		{
+			return Ok(pObj.GetinStockProducts(state));
+		}
+		[HttpGet]
+		[Route("/plist/total")]
+		public IActionResult GetTotalProducts()
+		{
+			return Ok(pObj.GetTotalProducts());
+		}
+
+
+
+		[HttpPost]
+		[Route("/plist/add")]
+		public IActionResult AddNewProducts([FromBody] Products prdobj)
+		{
+			var addresult = pObj.AddProducts(prdobj);
+			return Created("", addresult);
+		}
+		[HttpDelete]
+		[Route("/plist/delete/{id}")]
+		public IActionResult DeleteProduct(int id)
+		{
+			try
+			{
+				var deleteResult = pObj.DeleteProduct(id);
+				return Accepted(deleteResult);
+			}
+			catch (Exception es)
+			{
+				return NotFound(es.Message);
+			}
+		}
+		[HttpPut]
+		[Route("/plist/edit/{id}")]
+		public IActionResult UpdateProduct(int id, [FromBody] Products prdObj)
+		{
+			try
+			{
+				var updateResult = pObj.UpdateProduct(id, prdObj);
+				return Ok(updateResult);
+			}
+			catch (Exception es)
+			{
+				return NotFound(es.Message);
+
+			}
+		}
+	}
+}
---
+++ shoppingAPI/shoppingAPI/Models/Products.cs
@@ -0,0 +1,119 @@
+﻿namespace shoppingAPI.Models
+{
+	public class Products
+	{
+		//pId, pName, pCategory, pPrice, pDescription, pIsInStock
+		#region Properties
+		public int pId { get; set; }
+
+		public string pName { get; set; }
+
+		public string pCategory { get; set; }
+		public double pPrice { get; set; }
+
+		public string pDescription { get; set; }
+
+		public bool pIsInStock { get; set; }
+
+		#endregion
+		#region Data
+
+		private static int prd_autoNo = 0;
+
+		public Products()
+		{
+			prd_autoNo = prd_autoNo + 1;
+			this.pId = prd_autoNo;
+		}
+
+		private static List<Products> pList = new List<Products>()
+			{
+				new Products(){ pName="Kitkat", pCategory="Choculate", pPrice=200,  pDescription="Sales",    pIsInStock=true },
+				new Products(){ pName="dairymilk", pCategory="Choculate", pPrice=100,   pDescription="Sales", pIsInStock=true } ,
+				new Products(){ pName="milkybar", pCategory="Choculate", pPrice=120, pDescription="Sales",    pIsInStock=false   },
+				new Products(){ pName="fivestar", pCategory="Choculate", pPrice=200, pDescription="Sales",    pIsInStock=true   },
+				new Products(){ pName="munch", pCategory="Choculate", pPrice=150, pDescription="Sales", pIsInStock=true },
+				new Products(){ pName="oreo", pCategory="Biscuit", pPrice=100,  pDescription="Sales", pIsInStock=true } ,
+				new Products(){ pName="Dark Fantacy", pCategory="Biscuit", pPrice=120, pDescription="Sales",    pIsInStock=false   },
+				new Products(){ pName="Kiran", pCategory="Biscuit", pPrice=200, pDescription="Sales",    pIsInStock=true    },
+				new Products(){ pName="apple", pCategory="fruit", pPrice=100, pDescription="Sales", pIsInStock=true },
+				new Products(){ pName="mango", pCategory="fruit",pPrice=180, pDescription="Sales",    pIsInStock=false   },
+			};
+		#endregion
+		#region Methods
+
+		#region Get Methods
+		public List<Products> GetAllProducts()
+		{
+			return pList;
+		}
+		public Products GetProductsByName(string name)
+		{
+			var prd = pList.Find(p => p.pName == name);
+			if (prd != null)
+			{
+				return prd;
+			}
+			throw new Exception("pIsInStock Not Found");
+		}
+		
+		public Products GetProductsByCategory(string category)
+		{
+			var prd = pList.Find(p => p.pCategory == category);
+			if (prd != null)
+			{
+				return prd;
+			}
+			throw new Exception("pIsInStock Not Found");
+		}
+		public List<Products> GetinStockProducts(bool state)
+		{
+			var prd = pList.FindAll(p => p.pIsInStock == state);
+			return prd;
+		}
+		public int GetTotalProducts()
+		{
+			var total = pList.Count;
+			return total;
+		}
+		#endregion
+
+		#region Add, update and delete
+		public string AddProducts(Products newPrdObj)
+		{
+			pList.Add(newPrdObj);
+			return "Products Added to list, new Product number is : " + newPrdObj.pId;
+		}
+
+		public string UpdateProduct(int id, Products prdObj)
+		{
+			var prd = pList.Find(p => p.pId == id);
+
+			if (prd != null)
+			{
+				prd.pName = prdObj.pName;
+				prd.pCategory = prdObj.pCategory;
+				prd.pPrice = prdObj.pPrice;
+				prd.pDescription = prdObj.pDescription;
+				prd.pIsInStock = prdObj.pIsInStock;
+
+				return "Product Updated";
+			}
+			return "product not Found, update failed";
+		}
+		public string DeleteProduct(int id)
+		{
+			var prd = pList.Find(p => p.pId == id);
+			if (prd != null)
+			{
+				pList.Remove(prd);
+				return "Product Deleted";
+			}
+			throw new Exception("Product Not Found");
+
+		}
+		#endregion
+		#endregion
+	}
+}
+
